e1221a0b527012456090679795b701765b5c41cf
Alvaro <alvarofleith@gmail.com>
2017-10-12 22:41:03 -0300

    * Change view to render an HTML file instead a fixed string. * Add lists to APPs on Django configuration file * Create a template file for home.html

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -14,7 +14,7 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        html = response.content.decode('utf8')
+        html = response.content.decode('utf8').strip()
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.endswith('</html>'))
