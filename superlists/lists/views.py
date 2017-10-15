570a5f4a664d7b4d8b70c887440b74dbf9f3744f
Alvaro <alvarofleith@gmail.com>
2017-10-15 07:58:56 -0200

    Save items to the DB

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from .models import Item
+
+
 def home_page( request ):
-    # if request.method == 'POST':
-    #     return HttpResponse(request.POST['item_text'])
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+    else:
+        new_item_text = ''
+
     return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
+        'new_item_text': new_item_text,
     })
-    # return HttpResponse("<html><title>To-Do lists</title><body>My fat body!</body></html>")
+
 
