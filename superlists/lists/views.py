123b1e8faf58c8b33db275bce8ff5f77bf871ff7
Alvaro <alvarofleith@gmail.com>
2017-10-22 08:01:10 -0200

    Url to create new lists

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -6,9 +6,6 @@ from .models import Item
 
 
 def home_page( request ):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 
@@ -17,3 +14,6 @@ def view_list( request ):
     return render(request, 'list.html', {'items': items})
 
 
+def new_list( request ):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
